386f22c488d56fea818a3cc5b56a8145151e3af2
crisro <crisro381@gmail.com>
2025-08-13 01:37:16 +0330

    Refactor ZsignHandler to simplify signing method calls and remove unused completion handlers. Update InstallPreviewView to streamline installation process by removing unnecessary parameters.

--- Feather/Views/Library/Install/InstallPreviewView.swift
+++ Feather/Views/Library/Install/InstallPreviewView.swift
@@ -113,8 +113,8 @@ struct InstallPreviewView: View {
 							viewModel.status = .ready
 						}
 					} else if await _installationMethod == 1 {
-						let handler = await InstallationProxy(viewModel: viewModel)
-						try await handler.install(at: packageUrl, suspend: app.identifier == Bundle.main.bundleIdentifier!)
+                        let handler = await InstallationProxy(viewModel: viewModel)
+                        try await handler.install(at: packageUrl)
 					}
 				} else {
 					let package = try await handler.moveToArchive(packageUrl, shouldOpen: !_useShareSheet)
